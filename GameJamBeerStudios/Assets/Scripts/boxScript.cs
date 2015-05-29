5e1c6a64bcebceee7ec57f0d28d2ed526169fd60
Manuel Cruz Ram�rez <manuel.cruz@live.u-tad.com>
2015-05-29 20:27:22 +0200

    inicio cajas

---
+++ GameJamBeerStudios/Assets/Scripts/boxScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boxScript : MonoBehaviour {
+
+	public int life = 1;
+	public bool hasDrop = false;
+
+	public enum dropItems{
+		beer,
+	};
+	
+	public dropItems drop = dropItems.beer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == Tags.ball) {
+			life --;
+			if(life <= 0) {
+				if(hasDrop){
+					switch(drop){
+					case dropItems.beer:
+						//Instantiate
+						break;
+					}
+				}
+
+				Destroy (this);
+			}
+		}
+
+	}
+}
