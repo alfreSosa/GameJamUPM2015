39196f79e5d8aa9c1232464ae90bc21c00b81a94
Alfredo Sosa <alfres92@gmail.com>
2015-05-29 19:52:54 +0200

    Script tags

---
+++ GameJamBeerStudios/Assets/Scripts/Tags.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags {
+	public const string player = "Player";
+	public const string camera = "MainCamera";
+	public const string world = "World";
+	public const string floor = "Floor";
+	public const string enemy = "Enemy";
+	public const string axis = "Axis";
+	public const string fader = "Fader";
+	public const string playerFeet = "PlayerFeet";
+	public const string gameManager = "GameManager";
+	public const string key = "Key";
+	public const string altar = "Altar";
+}
