d4deb9b096732d1a109ccbdf2127bc4e5e2e30a7
Alfredo Sosa <alfres92@gmail.com>
2015-05-29 19:57:45 +0200

    Tags iniciales.

--- GameJamBeerStudios/Assets/Scripts/Tags.cs
+++ GameJamBeerStudios/Assets/Scripts/Tags.cs
@@ -3,14 +3,8 @@ using System.Collections;
 
 public class Tags {
 	public const string player = "Player";
-	public const string camera = "MainCamera";
-	public const string world = "World";
-	public const string floor = "Floor";
-	public const string enemy = "Enemy";
-	public const string axis = "Axis";
-	public const string fader = "Fader";
-	public const string playerFeet = "PlayerFeet";
+	public const string box = "Box";
+	public const string item = "Item";
+	public const string ball = "Ball";
 	public const string gameManager = "GameManager";
-	public const string key = "Key";
-	public const string altar = "Altar";
 }
