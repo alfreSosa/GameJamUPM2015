3d6ff7946904405497feb50f6b7f8193a530b63b
Alfredo Sosa <alfres92@gmail.com>
2015-05-29 22:21:36 +0200

    Movimiento bola y player empezado.

---
+++ GameJamBeerStudios/Assets/Scripts/Ball/BallMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	public float speedMovement = 5.0f;
+	public float desviation = 0.1f;
+	private Rigidbody2D rb2D;
+	private bool initMovement = false;
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void FixedUpdate() {
+		if (initMovement)
+			rb2D.AddForce(transform.up * speedMovement);
+	}
+
+	public void InitMovement() {
+		initMovement = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		GameObject ball = collision.gameObject;
+		if (ball.tag != Tags.item) {
+			Vector3 eulerAngles = ball.transform.eulerAngles;
+			Vector3 newEulerAngles = new Vector3(eulerAngles.x, eulerAngles.y, -eulerAngles.z);
+			ball.transform.eulerAngles = newEulerAngles;
+		}
+	}
+}
--- GameJamBeerStudios/Assets/Scripts/Player/MovementCube.cs
+++ GameJamBeerStudios/Assets/Scripts/Player/MovementCube.cs
@@ -4,14 +4,24 @@ using System.Collections;
 public class MovementCube : MonoBehaviour {
 
 
-	public float speedMovement = 50.0f;
+	public float speedMovement = 10.0f;
+	public float max_distance = 10.0f;
 
+	private BallMovement m_movementBall;
 	void Start () {
+		GameObject ball = GameObject.FindGameObjectWithTag (Tags.ball);
+		m_movementBall = ball.GetComponent<BallMovement> ();
 	}
 
 	void Update () {
 		float translation = Input.GetAxis("HorizontalMovement") * speedMovement;
 		translation *= Time.deltaTime;
 		transform.Translate(translation, 0, 0);
+		if (transform.position.x >= max_distance)
+			transform.position = new Vector3 (max_distance, transform.position.y, 0);
+		if (transform.position.x <= -max_distance)
+			transform.position = new Vector3 (-max_distance, transform.position.y, 0);
+		if (Input.GetAxis ("Throw") != 0.0f)
+			m_movementBall.InitMovement ();
 	}
 }
