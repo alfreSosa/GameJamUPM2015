340e481d6968ad1933acfa64052beca1e792a48b
Alfredo Sosa <alfres92@gmail.com>
2015-05-29 20:27:43 +0200

    Player movement 1.

---
+++ GameJamBeerStudios/Assets/Scripts/Player/MovementCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementCube : MonoBehaviour {
+
+
+	public float speedMovement = 50.0f;
+
+	void Start () {
+	}
+
+	void Update () {
+		float translation = Input.GetAxis("HorizontalMovement") * speedMovement;
+		translation *= Time.deltaTime;
+		transform.Translate(translation, 0, 0);
+	}
+}
