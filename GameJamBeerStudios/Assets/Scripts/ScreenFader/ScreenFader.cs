ff399d9ddcf72fa169849583e802eb083b10fb95
Manuel Cruz Ram�rez <manuel.cruz@live.u-tad.com>
2015-05-30 16:48:54 +0200

    sdf

---
+++ GameJamBeerStudios/Assets/Scripts/ScreenFader/ScreenFader.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenFader : MonoBehaviour {
+	public float initialDealy = 2f;  
+	public float fadeInSpeed = 0.5f;  
+	public float fadeOutSpeed = 0.5f; 
+	private bool m_sceneStarting = true;  
+	private bool m_sceneEnding = false;  
+	private Image m_image;
+
+	private float m_timer = 0;
+
+	void Start (){
+		m_image = GetComponent<Image> ();
+	}
+	
+	void Update (){
+		if(m_sceneStarting)
+			StartScene();
+		if(m_sceneEnding)
+			EndScene();
+	}
+	
+	
+	void FadeToClear (){
+		m_image.color = Color.Lerp (m_image.color, Color.clear, fadeInSpeed * Time.deltaTime);
+	}
+	
+	
+	void FadeToBlack (){
+		m_image.color = Color.Lerp(m_image.color, Color.black, fadeOutSpeed * Time.deltaTime);
+	}
+	
+	void StartScene (){
+		m_timer += Time.deltaTime;
+
+		if (m_timer > initialDealy) {
+			FadeToClear ();
+		
+			if (m_image.color.a <= 0.05f) {
+				m_image.color = Color.clear;
+				m_image.enabled = false;
+			
+				m_sceneStarting = false;
+
+				m_timer = 0;
+			}
+		}
+	}
+	
+	public void End (){
+		m_sceneEnding = true;
+	}
+	
+	void EndScene (){
+		m_sceneStarting = false;
+		m_image.enabled = true;
+		
+		FadeToBlack();
+		
+		if (m_image.color.a >= 0.95f) {
+			//hacer cosas de fin
+		}
+	}
+}
---
+++ GameJamBeerStudios/Assets/Scripts/Utils/levelSelection.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelSelection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SelectLevel(int value) {
+		switch (value) {
+		case 1:
+			Application.LoadLevel ("nivel_1");
+			break;
+		case 2:
+			Application.LoadLevel ("nivel_2");
+			break;
+		case 3:
+			Application.LoadLevel ("nivel_3");
+			break;
+		case 4:
+			Application.LoadLevel ("nivel_4");
+			break;
+		case 5:
+			Application.LoadLevel ("nivel_5");
+			break;
+		case 6:
+			Application.LoadLevel ("nivel_6");
+			break;
+		case 7:
+			Application.LoadLevel ("nivel_boss");
+			break;
+		}
+	}
+
+	public void Exit(){
+		Application.Quit();
+	}
+}
