d8df372d2871d2a98e2f83bed0e0ab0a026439c8
Manuel Cruz Ram�rez <manuel.cruz@live.u-tad.com>
2015-05-30 09:35:50 +0200

    boss

---
+++ GameJamBeerStudios/Assets/Scripts/GameElements/MinionMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionMovement : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	private Vector3 vec0;
+	private Vector3 vec1;
+
+	// Use this for initialization
+	void Start () {
+		vec0 = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+		vec1 = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.nearClipPlane));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += -transform.up * speed * Time.deltaTime;
+
+		DestroyBoundaries ();
+	}
+
+	void DestroyBoundaries() {
+		if ((transform.position.x < vec0.x) || (transform.position.x > vec1.x) || (transform.position.y < vec0.y) || (transform.position.y > vec1.y))
+			Destroy(gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag == Tags.player) {
+			GameObject gameManager = GameObject.FindGameObjectWithTag (Tags.gameManager);
+			gameManager.GetComponent<GameManager> ().LoseLife();
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ GameJamBeerStudios/Assets/Scripts/GameElements/bossMovement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bossMovement : MonoBehaviour {
+	public int life = 6;
+	public int secondPhaseLife = 2;
+	public float distance = 5;
+	public float speed = 5.0f;
+
+	private bool m_secondPhase = false;
+	private Vector3 m_initialPosition;
+	private Vector3 m_initialVector;
+	private float m_currentDistance = 0.0f;
+	private int m_count = 1;
+
+	public GameObject minion;
+	public float timeToMinion = 1.0f;
+	private float m_countMinion = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		m_initialPosition = transform.position;
+		m_initialVector = transform.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		phase1 ();
+		if(m_secondPhase)
+			phase2 ();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == Tags.ball) {
+			life--;
+			if(life <= secondPhaseLife)
+				m_secondPhase = true;
+			if (life <= 0) {
+				
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void phase1(){
+		m_currentDistance += speed * Time.deltaTime;
+		if (m_currentDistance > distance) {
+			m_currentDistance = distance;
+		} 
+		transform.position = m_initialPosition + m_initialVector * m_currentDistance;
+		if (m_currentDistance == distance) {
+			m_currentDistance = 0;
+			transform.eulerAngles += new Vector3(0.0f, 0.0f, m_count * 120.0f);
+			m_initialVector = transform.right;
+			transform.eulerAngles -= new Vector3(0.0f, 0.0f, m_count * 120.0f);
+			m_count++;
+			if(m_count >= 3)
+				m_count = 0;
+			m_initialPosition = transform.position;
+		}
+	}
+
+	void phase2(){
+		m_countMinion += Time.deltaTime;
+		if (m_countMinion > timeToMinion) {
+			m_countMinion = 0;
+			Instantiate (minion, transform.position, transform.rotation);
+		} 
+	}
+}
